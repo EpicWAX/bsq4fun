46911c86867f7ccddda28e87790619a95be4dd39
EpicWAX <quentinhusler@gmail.com>
2018-09-04 04:10:20 +0200

    adding print function + cleaning dubug message

--- main.c
+++ main.c
@@ -1,18 +1,6 @@
 #include "includes/bsq.h"
 
-int    print_bsq_struct(t_env *e)
-{
-    printf("/****struct****\\\n");
-    printf("| sy 0 [%c]\n", e->sy[0]);
-    printf("| sy 1 [%c]\n", e->sy[1]);
-    printf("| sy 2 [%c]\n", e->sy[2]);
-    printf("| nu y [%d]\n", e->yx[0]);
-    printf("| nu x [%d]\n", e->yx[1]);
-    printf("\\**************/\n\n");
-    return (1);
-}
-
-int     struct_init(t_env *e, char *line)
+void	struct_init(t_env *e, char *line)
 {
     char *ptr;
 
@@ -26,12 +14,13 @@ int     struct_init(t_env *e, char *line)
     e->fp[0] = -1;
     e->fp[1] = -1;
     e->fp[2] = -1;
-    return (1);
 }
 
-int     not_legit(char empty, char block, char c)
+void		new_sq_init(int *fp, int y, int x, int size)
 {
-    return ((c == empty || c == block) ? 1 : 0);
+	fp[0] = y;
+	fp[1] = x;
+	fp[2] = size;
 }
 
 int         ret_min(int a, int b, int c)
@@ -41,7 +30,35 @@ int         ret_min(int a, int b, int c)
     return ((b < a) ? b : a);
 }
 
-void        bsq(int fd)
+void		print_solution(int ac, t_env *e)
+{
+	int y;
+	int x;
+
+	y = e->fp[0] + 1;
+	while (--y != (e->fp[0] - e->fp[2]) && (x = e->fp[1] + 1))
+		while (--x != (e->fp[1] - e->fp[2]))
+			e->map[y][x] = -1;
+    y = -1;
+    while (++y < e->yx[0])
+    {
+		x = -1;
+        while (++x < e->yx[1])
+        {
+			if (e->map[y][x] == -1)
+				ft_putchar(e->sy[2]);
+			else if (e->map[y][x] == 0)
+				ft_putchar(e->sy[1]);
+			else
+				ft_putchar(e->sy[0]);
+        }
+        ft_putchar('\n');
+    }
+	if (ac != 1)
+		ft_putchar('\n');
+}
+
+void        bsq(int ac, int fd)
 {
     t_env   e;
     char    *line;
@@ -55,7 +72,7 @@ void        bsq(int fd)
     struct_init(&e, line);
     i = 0;
     y = e.yx[0];
-    e.map = ft_memalloc(y);
+	e.map = (int**)malloc(sizeof(int*) * y);
     while (y--)
     {
         get_next_line(fd, &line);
@@ -64,47 +81,29 @@ void        bsq(int fd)
         if (e.yx[1] != (int)ft_strlen(line))
             return (ft_putendl("bad len"));
         e.map[i] = (int*)malloc(sizeof(int) * e.yx[1]);
-        // e.map[i] = ft_memalloc(x);
         x = -1;
         while (++x < e.yx[1])
         {
-            if (not_legit(e.sy[0], e.sy[1], line[x]) == 0)
+			if (line[x] != e.sy[0] && line[x] != e.sy[1])
                 return (ft_putendl("invalid char"));
             if (i == 0 || x == 0)
                 e.map[i][x] = (line[x] == e.sy[0]) ? 1 : 0;
             else if (i > 0 && x > 0)
-                e.map[i][x] = (line[x] == e.sy[1]) ? 0 : ret_min(e.map[i][x - 1], e.map[i - 1][x - 1], e.map[i - 1][x]) + 1;
-            printf("[%d] ", e.map[i][x]);
-            if (line[x] != e.sy[1] && e.map[i][x] > e.fp[2])
-            {
-                e.fp[2] = e.map[i][x];
-                e.fp[0] = i;
-                e.fp[1] = x;
-            }
+                e.map[i][x] = (line[x] == e.sy[1]) ? 0 :
+					ret_min(e.map[i][x - 1], e.map[i - 1][x - 1], e.map[i - 1][x]) + 1;
+            if (e.map[i][x] > e.fp[2] && line[x] != e.sy[1])
+				new_sq_init(e.fp, i, x, e.map[i][x]);
         }
-        printf("\n");
         i++;
     }
-    printf("\nsuite test\nbsq y = %d, x = %d, size = %d\n\n", e.yx[0], e.yx[1], e.yx[2]);
-    y = -1;
-    while (++y < e.yx[0])
-    {
-        x = -1;
-        while (++x < e.yx[1])
-        {
-            // if (y == e.fp[0] && x == e.yx[1])
-                // e.map[y][x] = 123;
-            printf("[%d] ", e.map[y][x]);
-        }
-        printf("\n");
-    }
     close(fd);
+	print_solution(ac, &e);
 }
 
 int     main(int ac, char **av)
 {
     if (ac > 1 && *av++)
         while (ac-- > 1)
-           bsq(open(*av++, O_RDONLY));
+           bsq(ac, open(*av++, O_RDONLY));
     return (0);
 }
